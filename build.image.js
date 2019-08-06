1f93bcd0aeea447b99cea4d9b9ac6c2221b49ed6
YouzheJ <946008612@qq.com>
2019-08-06 19:37:16 +0800

    update build.image.js

--- build.image.js
+++ build.image.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // node > 8.4
-require('shelljs/global');
+const shell = require('shelljs');
 // 更新业务代码
 
 // 使用package.json中的配置
@@ -10,43 +10,42 @@ let tag = packageJson.version;
 let imageName = packageJson.imageName;
 let imageHubName = packageJson.imageHubName;
 
-
 let argTag = process.argv[2];
 if (argTag) {
-    echo('==============================================================');
-    echo();
-    echo('WARNING! arg1 VERSION is present, use this TAG instead of version in package.json.');
-    echo();
-    echo('==============================================================');
+    shell.echo('==============================================================');
+    shell.echo();
+    shell.echo('WARNING! arg1 VERSION is present, use this TAG instead of version in package.json.');
+    shell.echo();
+    shell.echo('==============================================================');
 }
 
 if (!tag) {
-    echo('ERROE! VERSION tag is Null. ');
-    exit(1);
+    shell.echo('ERROE! VERSION tag is Null. ');
+    shell.exit(1);
 }
 
-echo();
-echo(`IMAGE NAME: ${imageName}`);
-echo(`HARBOR IMAGE NAME: ${imageHubName}`);
-echo(`VERSION: ${tag}`);
-echo();
-echo('========== build start ==========');
+shell.echo();
+shell.echo(`IMAGE NAME: ${imageName}`);
+shell.echo(`HARBOR IMAGE NAME: ${imageHubName}`);
+shell.echo(`VERSION: ${tag}`);
+shell.echo();
+shell.echo('========== build start ==========');
 
-cd('./');
-exec('cnpm install');
-exec('npm run build:proxy');
+shell.cd('./');
+shell.exec('cnpm install');
+shell.exec('npm run build:proxy');
 
-rm('./build.tar.gz');
-exec('tar -zcvf build.tar.gz dist/* proxy/*');
-rm('-rf', './build-image/dist/', './build-image/proxy/', './build-image/build.tar.gz');
-mv('./build.tar.gz', './build-image/');
+shell.rm('./build.tar.gz');
+shell.exec('tar -zcvf build.tar.gz dist/* proxy/*');
+shell.rm('-rf', './build-image/dist/', './build-image/proxy/', './build-image/build.tar.gz');
+shell.mv('./build.tar.gz', './build-image/');
 
-cd('./build-image/');
-exec('tar -xf build.tar.gz');
+shell.cd('./build-image/');
+shell.exec('tar -xf build.tar.gz');
 
-exec(`docker login -u ${harborConfig.user} --password ${harborConfig.password} ${harborConfig.host}`);
-exec(`docker rmi ${imageName}:${tag}`);
-exec(`docker rmi ${imageHubName}:${tag}`);
-exec(`docker build -t ${imageName}:${tag} .`);
-exec(`docker tag ${imageName}:${tag} ${imageHubName}:${tag}`);
-exec(`docker push ${imageHubName}:${tag}`);
+shell.exec(`docker login -u ${harborConfig.user} --password ${harborConfig.password} ${harborConfig.host}`);
+shell.exec(`docker rmi ${imageName}:${tag}`);
+shell.exec(`docker rmi ${imageHubName}:${tag}`);
+shell.exec(`docker build -t ${imageName}:${tag} .`);
+shell.exec(`docker tag ${imageName}:${tag} ${imageHubName}:${tag}`);
+shell.exec(`docker push ${imageHubName}:${tag}`);
